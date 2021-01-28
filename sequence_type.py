8f6224ccf430f7348ebfb92f323a3fb232f181fa
appleboy919 <doh24@wisc.edu>
2021-01-28 16:45:39 +0900

    immutable tuples

--- sequence_type.py
+++ sequence_type.py
@@ -1,8 +1,12 @@
 x = [1, 2, 3, 4]  # list is created with [] brackets
-x[0] = 0 # list is mutable
+x[0] = 0  # list is mutable
 for i in x:
     print("i is {}".format(i))
 
+y = (1, 2, 3, 4)  # tuple is created with () parentheses
+for j in y:
+    print("j is {}".format(j))
+y[0] = 0  # this line is an error -> tuple is immutable
 
 ### NOTE ###
 # 1: List is mutable -> can reassign

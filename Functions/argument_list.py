492930a6a630077a90da4652e8cb1fe24787c407
appleboy919 <doh24@wisc.edu>
2021-01-30 10:14:10 +0900

    keyword arguments

--- Functions/argument_list.py
+++ Functions/argument_list.py
@@ -6,8 +6,21 @@ def arg_list(*args):
         print("Empty list")
 
 
+def kwarg_func(**kwargs):
+    if len(kwargs):
+        for k in kwargs:
+            print('{}, {}'.format(k, kwargs[k]))
+    else:
+        print('empty dictionary')
+
+
 arg_list(1, 2, 3)
 x = (1, 3, 5, 7)
 y = []
 arg_list(*x)
 arg_list(*y)
+
+kwarg_func(one='1', two='2', three='3')
+a = {'4': 'four', '5': 'five', '6': 'six'}
+kwarg_func(**a)
+kwarg_func()
